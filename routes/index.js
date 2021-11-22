350dda8594d170a3e1eacd3534d9673450d0e4fb
Maks Klimov <maksklimovbr@gmail.com>
2021-11-22 17:22:58 +0200

    + about router and view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Kaliningrad Places" });
 });
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About project"});
+})
 
 module.exports = router;
