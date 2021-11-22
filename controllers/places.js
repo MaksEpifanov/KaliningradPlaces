f88eadb4839ba5e8e4ab47a3cbe393e7ab6ca105
Maks Klimov <maksklimovbr@gmail.com>
2021-11-22 12:49:59 +0200

    adding logic place controllers

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,70 @@
 const Place = require('../models/place');
 
+// TODO Cloudinary images
 
-module.exports.index = async(req, res) => {
+// TODO mapbox geocoding
+
+module.exports.index = async (req, res, next) => {
   const places = await Place.find({})
-  res.render()
+  res.render('places/index', { places, title: 'All places in Kaliningrad'})
+};
+
+module.exports.renderNewForm = (req, res, next) => {
+  res.render('places/new', { title: 'Create new place'})
+};
+
+module.exports.createPlace = async (req, res, next) => {
+
+  // TODO add geoData with geocoding
+
+  const place = new Place(req.body.place);
+  place.images = req.files.map(file => ({url: file.path, filename: file.filename}))
+  place.author = req.user._id;
+
+  // TODO place.geometry add
+
+  await place.save();
+  req.flash('success', 'Success your create new place')
+  res.redirect(`places/${place._id}`);
+};
+
+module.exports.showPlace = async (req, res, next) => {
+  const { id } = req.params;
+  const place = await Place.findById(id)
+    .populate({
+      path: 'reviews',
+      populate: {
+        path: 'author'
+      }
+    })
+    .populate('author');
+  if (!place) {
+    req.flash('error', 'Not find place')
+    res.redirect('/places');
+  }
+  res.render('places/show', { title: `${place.title}`, place, id})
+};
+
+module.exports.renderEditForm = async (req, res, next) => {
+  const { id } = req.params;
+  const place = await Place.findById(id)
+  res.render('places/edit', { title: `Edit ${place.title}`, place})
+}
+
+module.exports.updatePlace = async (req, res, next) => {
+  const { id } = req.params;
+  const place = await Place.findByIdAndUpdate(id, req.body.place)
+  const files = req.files.map(file => ({ url: file.path, filename: file.filename}))
+  await place.images.push(...files);
+  await place.save();
+  // TODO delete cloudinary image
+  req.flash('success', 'Success your update place')
+  res.redirect(`/places/${id}`)
+}
+
+module.exports.deletePlace = async (req, res, next) => {
+  const { id } = req.params
+  await Place.findByIdAndDelete(id);
+  req.flash('success', 'Success your delete place')
+  res.redirect('/places')
 }
