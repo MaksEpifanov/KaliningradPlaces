4585d87a75d1f4254e165cc2447a9171f282e171
Maks Klimov <maksklimovbr@gmail.com>
2021-11-22 13:19:17 +0200

    edit seeds index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Place = require('../models/place');
 const cities = require('./locations');
-const { places, description } = require('./seedsHelp')
+const { places, descriptors } = require('./seedsHelp')
 
 main().catch((err) => console.log(err));
 
@@ -15,7 +15,7 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const randomCity = Math.floor(Math.random() * 1000);
     const place = new Place({
-      title: `${sample(description)} ${sample(places)}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[randomCity].city}, ${cities[randomCity].state}`,
       // TODO author
       description: 'Cupidatat consectetur laboris non sit aute nisi pariatur elit ipsum officia laboris consequat nisi ad. Et magna pariatur aliquip laborum reprehenderit laboris ut eu eu nisi aliquip aliquip esse eiusmod. Incididunt sint cillum do laborum deserunt laborum magna esse ut proident fugiat quis. Velit sit ad nostrud sint commodo proident minim reprehenderit proident tempor est. Fugiat exercitation mollit mollit laborum ea. Laboris laborum pariatur sit nulla tempor anim mollit commodo. Elit sint nisi duis ad mollit.',
