ab96b9f6aba6eca2094b0d4c057851255ca30db0
Maks Klimov <maksklimovbr@gmail.com>
2021-11-29 16:49:37 +0200

    + controllers reviews

---
+++ controllers/reviews.js
@@ -0,0 +1,25 @@
+const Places = require('../models/place');
+const Review = require('../models/review');
+
+//* Add review
+module.exports.addReview = async (req, res, next) => {
+  const { id } = req.params;
+  const place = await Places.findById(id);
+  const review = new Review(req.body.review);
+  review.author = req.user._id;
+  place.reviews.push(review);
+  await place.save();
+  await review.save();
+  req.flash('success', 'Your create new review');
+  res.redirect(`/palces/${place._id}`);
+};
+
+
+//* Delete review
+module.exports.deleteReview = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  await Places.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash('success', 'Your delete review');
+  res.redirect(`/places/${id}`);
+};
