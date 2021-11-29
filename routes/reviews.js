b9d0fb32bea861a3bd5fe5d7ea1738b983602323
Maks Klimov <maksklimovbr@gmail.com>
2021-11-29 16:53:31 +0200

    + router reviews

---
+++ routes/reviews.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const review = require('../controllers/reviews');
+
+//* Add review
+router.post('/', review.addReview);
+
+//* Delete review
+router.delete('/:reviewId', review.deleteReview);
+
+module.exports = router
